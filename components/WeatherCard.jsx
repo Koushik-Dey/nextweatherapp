9c3bca82228cb9f1733e5c97b9d0715a386aad5a
Koushik-Dey <kd89601@gmail.com>
2023-04-07 00:11:37 -0700

    complete responsive

--- components/WeatherCard.jsx
+++ components/WeatherCard.jsx
@@ -4,7 +4,7 @@ const WeatherCard = ({ data }) => {
   return (
     <>
       <div className="m-2 p-4">
-        <div className="grid grid-cols-2 place-items-center">
+        <div className="grid grid-cols-2 place-items-center md:w-[80%] lg:w-[50%] mx-auto">
           <div className="text-center">
             <Image
               src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
@@ -15,10 +15,10 @@ const WeatherCard = ({ data }) => {
             <p className="text-white text-xl">{data.weather[0].main}</p>
           </div>
           <div className="text-5xl text-white">
-            <p className="text-5xl tex-white">{data.main["temp"]} &#176;</p>
+            <p className="text-5xl tex-white">{data.main.temp.toFixed(0)} &#176;F</p>
           </div>
         </div>
-        <div className="mt-10 bg-black/50 p-4">
+        <div className="mt-10 bg-black/50 p-4 md:w-[80%] lg:w-[50%] mx-auto rounded-lg">
           <div className="text-center">
             <h2 className="text-2xl text-white">Weather in {data.name}</h2>
           </div>
